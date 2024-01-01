44e7af873bd0ebfa6dcde2b72992499de50b9818
boying <d4rkl19ht79@gmail.com>
2024-01-01 09:17:24 -0500

    saving changes

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,15 +8,21 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
+
     # path for about view
     path(route='about/', view=views.get_about, name='get_about'),
+
     # path for contact us view
     path(route='contact/', view=views.get_contact, name='get_contact'),
+
     # path for registration
+    path(route='register/', view=views.register_user, name='register'),
 
     # path for login
+    path(route='login/', view=views.login_request, name='login'),
 
     # path for logout
+    path(route='logout/', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
